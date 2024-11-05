ee0f73060724bc2a467b5f5da721f0a815895662
Ray Wojciechowski <ray@skillbird.de>
2024-11-05 15:49:48 +0100

    Add missing http methods to service

--- src/app/recipe/services/recipe.service.ts
+++ src/app/recipe/services/recipe.service.ts
@@ -2,8 +2,10 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Recipe, RecipeDTO} from '../models/Recipe.model';
 import {map, Observable} from 'rxjs';
-import {mapRecipeDTOToRecipe} from '../mappings/recipe.mappings';
+import {mapRecipeDTOToRecipe, mapRecipeToRecipeDTO} from '../mappings/recipe.mappings';
 
+
+type RecipeIdentifier = Recipe | string
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +23,55 @@ export class RecipeService {
         // folgende Zeile ist funktional identisch zur obigen Zeile
         // map(recipeDTOs => recipeDTOs.map(dto => mapRecipeDTOToRecipe(dto)))
       )
+  }
+
+
+  public getRecipe(id: string): Observable<Recipe> {
+    return this.http.get<RecipeDTO>(this.getUrl(id))
+      .pipe(
+        map(dto => mapRecipeDTOToRecipe(dto))
+      )
+  }
+
+
+  public addRecipe(recipe: Recipe): Observable<Recipe> {
+    const recipeDTO = mapRecipeToRecipeDTO(recipe);
+    return this.http.post<RecipeDTO>(this.getUrl(), recipeDTO)
+      .pipe(
+        // äquivalent zu map(dto => mapRecipeDTOToRecipe(dto)
+        map(mapRecipeDTOToRecipe)
+      )
+  }
+
+
+  public updateRecipe(recipe: Recipe): Observable<Recipe> {
+    const recipeDTO = mapRecipeToRecipeDTO(recipe);
+    return this.http.put<RecipeDTO>(this.getUrl(recipe), recipeDTO)
+      .pipe(
+        map(mapRecipeDTOToRecipe)
+      )
+  }
+
+
+  public deleteRecipe(recipe: Recipe): Observable<void> {
+    return this.http.delete<void>(this.getUrl(recipe))
+  }
+
+
+  private getUrl(identifier?: RecipeIdentifier): string {
+    let id = this.extractID(identifier)
+
+    return `http://localhost:3000/recipes${id ? '/' + id : ""}`
+  }
+
+
+  private extractID(identifier?: RecipeIdentifier): string | undefined {
+    if (!identifier) return undefined
+
+    if (typeof identifier === "string") {
+      return identifier
+    }
+
+    return identifier.id
   }
 }
